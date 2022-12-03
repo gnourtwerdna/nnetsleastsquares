9f6c009b270fee936264aae5b66898cca4af0e4f
Andrew Truong <andrewtruong@Andrews-Air.lan>
2022-12-02 21:12:21 -0800

    Testing

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ from util import *
 def main():
     X_train, y_train = util.get_train_data()
     X_test, y_test = util.get_train_data(train=False)
-    constant = 1e-4
+    lambda_ = 1e-4
     weights = np.random.rand(16, 1)
-    j = jacobian(X_train, weights)
-    Dh = LM_matrix(j, constant)
-    check = func(X_train, weights)
-    temp = sse(X_train, y_train, weights)
+    temp = LM(X_train, y_train, weights, lambda_)
     print(temp)
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -26,6 +26,19 @@ def get_train_data(train=True):
     return X, y
 
 def f(x, w):
+    '''
+    Computes f(x).
+
+    Parameters
+    ----------
+    x: 3x1 matrix
+    w: 16x1 matrix
+
+    Returns
+    -------
+    float
+        Value after calculating f(x)
+    '''
     f = (w[0]*tanh(w[1]*x[0] + w[2]*x[1] + w[3]*x[2] + w[4]) 
         + w[5]*tanh(w[6]*x[0] + w[7]*x[1] + w[8]*x[2] + w[9])
         + w[10]*tanh(w[11]*x[0] + w[12]*x[1] + w[13]*x[2] + w[14])
@@ -33,10 +46,36 @@ def f(x, w):
     return f
 
 def func(X, w):
+    '''
+    Applies f(x) on the dataset.
+
+    Parameters
+    ----------
+    X: 500x3 matrix
+    w: 16x1 matrix
+
+    Returns
+    -------
+    np.array
+        500x1
+    '''
     return np.array([f(x, w) for x in X])
 
 def sse(X, y, w):
-    return np.sum(np.square(func(X, w) - y))
+    '''
+    Calculates the sum of squared errors. Σ(f(x)-y)^2
+
+    Parameters
+    ----------
+    X: 500x3 matrix
+    y: 500x1 matrix
+    w: 16x1 matrix
+
+    Returns
+    -------
+    float
+    '''
+    return np.square(func(X, w) - y)
 
 
 def grad(x, w):
@@ -78,13 +117,18 @@ def jacobian(X, w):
     Parameters
     ----------
     X: 500x3 matrix
+    w: 16x1 matrix
 
     Returns
     -------
     matrix
        500x16 matrix of gradients.
     '''
-    return np.array([grad(x, w) for x in X])
+    j = []
+    for x in X:
+        g = grad(x, w)
+        j.append(g)
+    return j
 
 def LM_matrix(j, constant):
     '''
@@ -102,6 +146,28 @@ def LM_matrix(j, constant):
     h = np.sqrt(constant)*np.identity(16)
     return np.concatenate((j, h))
 
+def calculate_b(X, y, w):
+    '''
+    Calculates b.
+
+    Parameters
+    ----------
+    X: 500x3 matrix
+    y: 500x1 matrix
+    w: 16x1 matrix
+    j: 500x3 jacobian matrix
+
+    Returns
+    -------
+    np.array
+       516x1 matrix
+    '''
+    r = sse(X, y, w)
+    Dr = jacobian(X, w)
+    temp = r - np.dot(Dr, w)
+    #return np.concatenate((temp, np.zeros((16,1))))
+    return Dr
+
 def tanh(x):
     '''
     Computes the tanh function. f(x) = (e^x - e^(-x)) / (e^x + e^(-x))
@@ -115,7 +181,7 @@ def tanh(x):
     float
        Value after applying tanh function
     '''
-    return (np.exp(x)-np.exp(-x))/(np.exp(x)+np.exp(-x))
+    return (np.exp(x)-np.exp(-x)) / (np.exp(x)+np.exp(-x))
 
 def deriv_tanh(x):
     '''
@@ -148,9 +214,36 @@ def RMSE(y, t):
     '''
     return np.sqrt(np.square((y - t)).mean())
 
-def LM(X, y, w):
+def LM(X, y, w, lambda_):
     iterations = 1000
     trust = 1e-3
     trust_increase = 2
     trust_decrease = 0.8
+    Dr = jacobian(X, w)
+    Dh = LM_matrix(Dr, lambda_)
+    b = calculate_b(X, y, w)
+
+
+
+    '''
+    trust_sqrt = np.sqrt(trust)
+    temp = trust_sqrt * w
+    y_ = np.concatenate((b, temp)) # 532x1 matrix
+
+    temp = trust_sqrt * np.identity(16)
+    A = np.concatenate((Dh, temp)) # 532x16 matrix
+
+    #pinv = np.linalg.pinv(A)
+    '''
+
+
+
+
+    '''
+    for i in range(iterations):
+        # least squares
+        pinv = np.linalg.pinv(A)
+        h = np.dot(pinv, y)
+    '''
+    return b
 
